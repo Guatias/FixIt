fd8a21fb8fd5d11b9d884f7aca46825dc907f15f
Gustavo <gustavoos26@gmail.com>
2021-09-14 20:50:53 -0300

    Pagina de Registro Criada

--- app/src/main/java/com/example/fixit/MainActivity.java
+++ app/src/main/java/com/example/fixit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fixit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         TextView password = (TextView) findViewById(R.id.password);
 
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        MaterialButton registerbtn = (MaterialButton) findViewById(R.id.registerbtn);
 
         //admin and admin
 
@@ -34,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
                     //incorrect
                     Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent register_activity = new Intent(getApplicationContext(), register_activity.class);
+                startActivity(register_activity);
+
             }
         });
     }
---
+++ app/src/main/java/com/example/fixit/register_activity.java
@@ -0,0 +1,17 @@
+package com.example.fixit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+
+}

ec1729523c416626ae5f6b3fa95661b524fc76dc
Gustavo <gustavoos26@gmail.com>
2021-10-20 17:52:55 -0300

    Lógica do login funcionando

--- app/src/main/java/com/example/fixit/MainActivity.java
+++ app/src/main/java/com/example/fixit/MainActivity.java
@@ -1,25 +1,34 @@
 package com.example.fixit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
@@ -32,13 +41,30 @@ public class MainActivity extends AppCompatActivity {
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    //correct
-                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                } else {
-                    //incorrect
-                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+
+                String email_string = username.getText().toString();
+                String senha_string = password.getText().toString();
+
+                if (TextUtils.isEmpty(email_string)) {
+                    username.setError("Insira seu email");
+                    return;
                 }
+
+                if (TextUtils.isEmpty(senha_string)) {
+                    password.setError("Insira sua senha");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email_string,senha_string).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Usuario Autenticado com Sucesso", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Usuario ou Senha Inválido", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
